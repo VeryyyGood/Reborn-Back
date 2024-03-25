3ce8649c2291f17c3a65f38f236cb992653ae95f
persi0815 <jina_8151010@naver.com>
2024-03-26 00:57:35 +0900

    Feature: Swagger 적용(로그인+회원)

--- src/main/java/reborn/backend/user/controller/UserController.java
+++ src/main/java/reborn/backend/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package reborn.backend.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -12,18 +15,27 @@ import reborn.backend.global.api_payload.SuccessCode;
 import reborn.backend.user.jwt.JwtDto;
 import reborn.backend.user.service.UserService;
 
+@Tag(name = "회원", description = "회원 관련 api 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "로그아웃", description = "로그아웃하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER_2001", description = "로그아웃 되었습니다."),
+    })
     @DeleteMapping("/logout")
     public ApiResponse<Integer> logout(HttpServletRequest request) {
         userService.logout(request);
         return ApiResponse.onSuccess(SuccessCode.USER_LOGOUT_SUCCESS, 1);
     }
 
+    @Operation(summary = "토큰 재발급", description = "토큰을 재발급하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER_2002", description = "토큰 재발급이 완료되었습니다."),
+    })
     @PostMapping("/reissue")
     public ApiResponse<JwtDto> reissue(
             HttpServletRequest request
@@ -32,6 +44,10 @@ public class UserController {
         return ApiResponse.onSuccess(SuccessCode.USER_REISSUE_SUCCESS, jwt);
     }
 
+    @Operation(summary = "회원탈퇴", description = "회원 탈퇴하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER_2003", description = "회원탈퇴가 완료되었습니다."),
+    })
     @DeleteMapping("/me")
     public ApiResponse<Integer> deleteUser(Authentication auth) {
         userService.deleteUser(auth.getName());
