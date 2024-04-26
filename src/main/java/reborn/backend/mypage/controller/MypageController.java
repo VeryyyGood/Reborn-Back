2b7896a2caf3600b154d9d96bd1697032197596f
KangMinJun <alswns8081@kau.kr>
2024-04-26 21:06:12 +0900

    반려동물 조회의 패키지 이동 및 Review 구현

--- src/main/java/reborn/backend/mypage/controller/MypageController.java
+++ src/main/java/reborn/backend/mypage/controller/MypageController.java
@@ -1,11 +1,110 @@
 package reborn.backend.mypage.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reborn.backend.global.api_payload.ApiResponse;
+import reborn.backend.global.api_payload.SuccessCode;
+import reborn.backend.pet.converter.PetConverter;
+import reborn.backend.pet.domain.Pet;
+import reborn.backend.pet.domain.PetType;
+import reborn.backend.pet.dto.PetResponseDto.DetailPetDto;
+import reborn.backend.pet.service.PetService;
+import reborn.backend.reborn_15._2_remind.converter.RemindConverter;
+import reborn.backend.reborn_15._2_remind.domain.Remind;
+import reborn.backend.reborn_15._2_remind.dto.RemindResponseDto.SimpleRemindDto;
+import reborn.backend.reborn_15._2_remind.service.RemindService;
+import reborn.backend.user.domain.User;
+import reborn.backend.user.jwt.CustomUserDetails;
+import reborn.backend.user.service.UserService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "마이페이지", description = "마이페이지 관련 api 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mypage")
 public class MypageController {
+
+    private final UserService userService;
+    private final PetService petService;
+    private final RemindService remindService;
+
+    // 모든 Pet 가져오기
+            @Operation(summary = "반려동물 정보 조회 메서드", description = "반려동물 정보 목록을 조회하는 메서드입니다.")
+            @ApiResponses(value = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "PET_2001", description = "반려동물 정보 목록 조회가 완료되었습니다.")
+    })
+    @GetMapping("/list")
+    public ApiResponse<List<DetailPetDto>> getAllPet(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+
+        List<Pet> pets = petService.findAllByUserSortedByCreatedAt(user);
+
+        List<DetailPetDto> petDtos = pets.stream()
+                .map(PetConverter::toDetailPetDto)
+                .collect(Collectors.toList());
+
+        return ApiResponse.onSuccess(SuccessCode.PET_LIST_VIEW_SUCCESS, petDtos);
+    }
+
+    // 특정 Pet 가져오기
+    @Operation(summary = "특정 반려동물 정보 조회 메서드", description = "특정 반려동물 정보를 조회하는 메서드입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "PET_2002", description = "반려동물 정보 조회가 완료되었습니다.")
+    })
+    @GetMapping("/list/{pet-id}")
+    public ApiResponse<DetailPetDto> getDetailPet(
+            @PathVariable(name = "pet-id") Long id
+    ){
+        Pet pet = petService.findById(id);
+        DetailPetDto detailPetDto = PetConverter.toDetailPetDto(pet);
+
+        return ApiResponse.onSuccess(SuccessCode.PET_DETAIL_VIEW_SUCCESS, detailPetDto);
+    }
+
+    // RECONNECT에 대한 REVIEW
+    @Operation(summary = "특정 반려동물의 종류 조회 메서드", description = "특정 반려동물의 종류를 조회하는 메서드입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "PET_2004", description = "반려동물 정보 조회가 완료되었습니다.")
+    })
+    @GetMapping("/reconnect/{pet-id}")
+    public ApiResponse<PetType> getReconnect(
+            @PathVariable(name = "pet-id") Long id
+    ){
+        Pet pet = petService.findById(id);
+
+        return ApiResponse.onSuccess(SuccessCode.PET_RECONNECT_VIEW_SUCCESS, pet.getPetType());
+    }
+
+    // REMIND에 대한 REVIEW
+    @Operation(summary = "충분한 대화 나누기 내용 조회 메서드", description = "충분한 대화 나누기 내용 조회 메서드입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "PET_2005", description = "충분한 대화 나누기 내용 조회가 완료되었습니다.")
+    })
+    @GetMapping("/remind/{pet-id}")
+    public ApiResponse<List<SimpleRemindDto>> getRemind(
+            @PathVariable(name = "pet-id") Long id
+    ){
+        Pet pet = petService.findById(id);
+
+        List<Remind> reminds = remindService.findAllByPetSortedByDate(pet);
+        List<SimpleRemindDto> remindDtos = reminds.stream()
+                .map(RemindConverter::toSimpleRemindDto)
+                .collect(Collectors.toList());
+
+        return ApiResponse.onSuccess(SuccessCode.PET_REMIND_VIEW_SUCCESS, remindDtos);
+    }
+
+
+
 }
