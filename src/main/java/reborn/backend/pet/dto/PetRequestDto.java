9775803744398c66616085a47dc3183569167cf2
KangMinJun <alswns8081@kau.kr>
2024-05-01 00:04:15 +0900

    사용하지 않는 DetailPetReqDto 삭제

--- src/main/java/reborn/backend/pet/dto/PetRequestDto.java
+++ src/main/java/reborn/backend/pet/dto/PetRequestDto.java
@@ -34,39 +34,7 @@ public class PetRequestDto {
         @Schema(description = "반려동물 색깔(BLACK, BROWN, LIGHTBROWN, GRAY, WHITE")
         private String petColor;
 
-        @Schema(description = "반려동물 사진 url?")
-        private String petImage;
-    }
-
-    @Schema(description = "DetailPetReqDto")
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class DetailPetReqDto {
-        @Schema(description = "반려동물 id")
-        private Long petId;
-
-        @Schema(description = "반려동물 이름")
-        private String petName;
-
-        @Schema(description = "컨텐츠 상 진행중인 날짜")
-        private Integer rebornDate;
-
-        @Schema(description = "반려동물 기일")
-        @DateTimeFormat(pattern = "yyyy-MM-dd")
-        private LocalDate anniversary;
-
-        @Schema(description = "반려동물 종류(DOG, CAT)")
-        private String petType;
-
-        @Schema(description = "반료동물 종")
-        private String detailPetType;
-
-        @Schema(description = "반려동물 색깔(BLACK, BROWN, LIGHTBROWN, GRAY, WHITE")
-        private String petColor;
-
-        @Schema(description = "반려동물 사진 url?")
+        @Schema(description = "반려동물 사진 url")
         private String petImage;
     }
 }
